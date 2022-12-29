c7571171ac5ee8cc01b4fc2552db907fdc75fafe
ChiranjeeviNalmas <20bd1a0574kmit@gmail.com>
2022-12-29 13:15:37 +0530

    added

---
+++ src/main/java/kmit/ExamMaven/App.java
@@ -0,0 +1,13 @@
+package kmit.ExamMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab externalexam; The created project is ExamMaven." );
+    }
+}
